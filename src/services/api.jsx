2f86d6110db1262849b3f46a12e8f11d600dd598
ali <85081010+ali-mohseni-moghadam@users.noreply.github.com>
2023-10-10 14:10:17 +0330

    Update api.jsx
    editing some code

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// const APIkey = "8293CE42-CEE7-47E3-8C3C-3EB4A238C0DD";
-
 const BASE_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en";
 
